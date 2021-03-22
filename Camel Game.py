ca9b80236225869f198a13806da31ddbeb46d4e4
hb16174 <hb16174@thameshigh.school.nz>
2021-03-23 09:45:52 +1300

    Start of CamelGame

---
+++ Camel Game.py
@@ -0,0 +1,11 @@
+import arcade
+
+
+def main():
+    print("""Welcome to cemlil gem!
+You have stolen a camel to make your way across the great Mobi dick desert.
+The natives want their camel back and are chasing you down! Survive your
+desert trek and outrun the natives.""")
+
+
+main()
